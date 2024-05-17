f007005229e274dc090bf493746be0a272ce06dd
Dusky <87861542+Dusky202@users.noreply.github.com>
2024-05-17 23:37:53 +0300

    202

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Dusky202
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnLock = new System.Windows.Forms.Button();
+            this.btnUnlock = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLock
+            // 
+            this.btnLock.BackColor = System.Drawing.SystemColors.Control;
+            this.btnLock.FlatAppearance.BorderSize = 0;
+            this.btnLock.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnLock.Image = global::Dusky202.Properties.Resources._2;
+            this.btnLock.Location = new System.Drawing.Point(-1, -1);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(183, 184);
+            this.btnLock.TabIndex = 1;
+            this.btnLock.UseVisualStyleBackColor = false;
+            this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
+            // 
+            // btnUnlock
+            // 
+            this.btnUnlock.FlatAppearance.BorderSize = 0;
+            this.btnUnlock.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnUnlock.Image = global::Dusky202.Properties.Resources._1;
+            this.btnUnlock.Location = new System.Drawing.Point(-1, -1);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(183, 184);
+            this.btnUnlock.TabIndex = 2;
+            this.btnUnlock.UseVisualStyleBackColor = false;
+            this.btnUnlock.Click += new System.EventHandler(this.btnUnlock_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnUnlock);
+            this.panel1.Controls.Add(this.btnLock);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(180, 181);
+            this.panel1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(180, 181);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Lock";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        private void SetCircularButton(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0;
+            button.Width = button.Height;
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, button.Width, button.Height);
+            button.Region = new Region(path);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLock;
+        private System.Windows.Forms.Button btnUnlock;
+        private Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Dusky202
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnLock.Visible = true;
+            SetCircularButton(btnLock);
+            SetCircularButton(btnUnlock);
+            RoundForm(this);
+        }
+
+        private void RoundForm(Form form)
+        {
+            // Formun yuvarlak köşeleri için bir GraphicsPath oluştur
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(new Rectangle(0, 0, form.Width, form.Height));
+
+            // Formun Region özelliğini yuvarlak yol ile ayarla
+            form.Region = new Region(path);
+        }
+
+        private void btnLock_Click(object sender, EventArgs e)
+        {
+            btnLock.Visible = false;
+            btnUnlock.Visible = true;
+            string folderName = "Desktop";
+            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folderName);
+
+            try
+            {
+                if (Directory.Exists(folderPath))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+                    dirInfo.Attributes |= FileAttributes.Hidden | FileAttributes.System;
+                    MessageBox.Show("Klasör başarıyla kilitlendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Belirtilen klasör bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Klasöre erişilirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Klasöre erişim yetkisi reddedildi: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Beklenmeyen bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnUnlock_Click(object sender, EventArgs e)
+        {
+            btnLock.Visible = true;
+            btnUnlock.Visible = false;
+            string folderName = "Desktop";
+            string folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, folderName);
+
+            try
+            {
+                if (Directory.Exists(folderPath))
+                {
+                    DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+                    dirInfo.Attributes &= ~(FileAttributes.Hidden | FileAttributes.System);
+                    MessageBox.Show("Klasör başarıyla kilidi açıldı.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Belirtilen klasör bulunamadı.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show($"Klasöre erişilirken bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Klasöre erişim yetkisi reddedildi: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Beklenmeyen bir hata oluştu: {ex.Message}", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
